e1d731ceeb6946c49b6e57ffda194d8d76969a27
pedroyorpo <pedroyorpo17@gmail.com>
2024-10-23 12:53:32 +0800

    first

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
 namespace BackendNETAPI.Controllers
 {
     [Route("api/[controller]")]
